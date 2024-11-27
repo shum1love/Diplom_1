9b0a23a63ddcf1d96d1dc33a282b8a26384cbf2b
Rodion Shumilov <rodya7911@gmal.com>
2024-11-27 23:03:34 +0300

    make test class for Ingredient

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -1,12 +1,20 @@
 package praktikum;
 
-import org.mockito.Mock;
-
+import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class IngredientTest {
-    @Mock
-    Ingredient ingredient;
 
+    // Тестирование конструктора и геттеров
+    @Test
+    public void testConstructorAndGetters() {
+        // Создаем стаб объекта Ingredient
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Ketchup", 50.0f);
 
+        // Проверяем, что значения правильно сохраняются и возвращаются геттерами
+        assertEquals(IngredientType.SAUCE, ingredient.getType());
+        assertEquals("Ketchup", ingredient.getName());
+        assertEquals(50.0f, ingredient.getPrice(), 0.01f);
+    }
 }
