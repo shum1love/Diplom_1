e315fa000681faf0e584486276c099cbf47a5bfe
Rodion Shumilov <rodya7911@gmail.com>
2024-12-11 12:57:28 +0300

    Внёс изменения в класс IngredientTest. Разделил 3 ассерта на разные проверки

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -7,13 +7,27 @@ public class IngredientTest {
 
     // Тестирование конструктора и геттеров
     @Test
-    public void testConstructorAndGetters() {
+    public void testConstructorAndGettersSauce() {
         // Создаем стаб объекта Ingredient
         Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Ketchup", 50.0f);
 
         // Проверяем, что значения правильно сохраняются и возвращаются геттерами
         assertEquals(IngredientType.SAUCE, ingredient.getType());
+    }
+    @Test
+    public void testConstructorAndGettersIngredient() {
+        // Создаем стаб объекта Ingredient
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Ketchup", 50.0f);
+
+        // Проверяем, что значения правильно сохраняются и возвращаются геттерами
         assertEquals("Ketchup", ingredient.getName());
+    }
+    @Test
+    public void testConstructorAndGettersPrice() {
+        // Создаем стаб объекта Ingredient
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Ketchup", 50.0f);
+
+        // Проверяем, что значения правильно сохраняются и возвращаются геттерами
         assertEquals(50.0f, ingredient.getPrice(), 0.01f);
     }
 }
